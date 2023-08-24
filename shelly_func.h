ecdd8f750e32261065573fb43348de37dcf18441
salpalmTree <salpalma123@gmail.com>
2023-08-23 22:23:16 -0600

    first commit

---
+++ shelly_func.h
@@ -0,0 +1,18 @@
+#ifndef _SHELLY_FUNC_H
+#define _SHELLY_FUNC_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ shelly_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shelly_func.h"
+
+
+int main()
+{
+
+
+    return 0; 
+}

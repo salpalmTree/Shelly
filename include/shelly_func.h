0ac7824b78a88baa5d56ba619e47ee0b3adf297d
salpalmTree <salpalma123@gmail.com>
2023-08-26 01:18:28 -0600

    First working prototype.

--- include/shelly_func.h
+++ include/shelly_func.h
@@ -1,26 +1,25 @@
 #ifndef _SHELLY_FUNC_H
 #define _SHELLY_FUNC_H
 
-typedef enum command {
-    ERR = 0,
+enum command {
+    EXT = 0,
     DELETE,
     CREATE, 
-    OPEN,
-    CLOSE,
-    EDIT, 
-    SHOW,
-}command; 
+    READ,
+    EDIT,
+    CONT,
+}commands; 
 
 
 // read input
 char * getInput()
 {
-    char *theInputString = (char *)malloc(sizeof(char) * 50);  
+    char *theInputString = (char *)malloc(sizeof(char) * 81);  
     if(theInputString == NULL) return NULL; 
     char *sIn = theInputString;
-    while((*sIn++ = getchar()) != '\n');
-    *sIn++ = '\0'; 
-    return theInputString; 
+    while((*theInputString++ = getchar()) != '\n');
+    *theInputString = '\0'; 
+    return sIn; 
 }
 // string comparison function
 bool equalStrings(char *stringOne, char *stringTwo)
@@ -30,58 +29,59 @@ bool equalStrings(char *stringOne, char *stringTwo)
     { 
         stringOne++; 
         stringTwo++; 
-        if(*stringOne == '\0' && *stringTwo == '\0')
-        {
-            areEqual = true; 
-        }
-        else
-        {
-            areEqual = false; 
-        }
+    }
+    if(*stringOne == '\0' && *stringTwo == '\0')
+    {
+        areEqual = true; 
+    }
+    else
+    {
+        areEqual = false; 
     }
     return areEqual; 
 }
 // sets command type
-command commandType(char *userInputString)
+enum command commandType(char *userInputString)
 {
-    if(equalStrings(userInputString, "delete"))
+    if(equalStrings(userInputString, "delete\n"))
     {
         return DELETE; 
     }
-    else if(equalStrings(userInputString, "create"))
+    else if(equalStrings(userInputString, "create\n"))
     {
         return CREATE;
     }
-    else if(equalStrings(userInputString, "open"))
+    else if(equalStrings(userInputString, "read\n"))
     {
-        return OPEN; 
+        return READ; 
     }
-    else if(equalStrings(userInputString, "close"))
-    {
-        return CLOSE;
-    }
-    else if(equalStrings(userInputString, "edit"))
+    else if(equalStrings(userInputString, "edit\n"))
     {
         return EDIT;
     }
-    else if(equalStrings(userInputString, "show"))
+    else if(equalStrings(userInputString, "exit\n"))
     {
-        return SHOW; 
+        return EXT; 
     }
-    return ERR; 
-}
-// file creation
-void createFile(char *fileName)
-{
-    FILE *newFile; 
-    if((newFile = fopen(fileName, "w")) == NULL) printf("File could not be created.\n"); 
     else
     {
-        printf("File [%s] has been created.\n", fileName); 
+        return CONT; 
+    }
+}
+// file creation
+FILE * createFile(char *fileName)
+{
+    FILE *newFile, *copyNewFile; 
+    if((newFile = fopen(fileName, "w")) == NULL){printf("File could not be created.\n");return NULL;}
+    else
+    {
+        copyNewFile = newFile; 
+        printf("A file has been created %s", fileName); 
         printf("To file ->"); 
         char *userIn = getInput(); 
         fputs(userIn, newFile); 
         fclose(newFile);
+        return copyNewFile; 
     }
 }
 // file deletion
@@ -92,6 +92,43 @@ void deleteFile(char *fileName)
         printf("Could not delete that file.\n"); 
     }
 }
-
-
+// read file
+void readFile(char *fileName, FILE * file)
+{
+    if((fopen(fileName, "r")) == NULL) printf("File doesn't exit.\n"); 
+    else
+    {
+        int c; 
+        while((c = getc(file)) != EOF)
+        {
+            putchar(c); 
+        }
+        fclose(file); 
+    }
+}
+// edit file (append)
+FILE * editFile(char *fileName)
+{
+    FILE *fileToAppend, *copyFileToAppend; 
+    if((fileToAppend = fopen(fileName, "a")) == NULL){printf("That file could not be edited.\n");return NULL;}
+    else
+    {
+        copyFileToAppend = fileToAppend; 
+        printf("File being appended %s", fileName); 
+        printf("To File ->"); 
+        char *userIn = getInput(); 
+        fputs(userIn, copyFileToAppend); 
+        fclose(copyFileToAppend); 
+        return copyFileToAppend; 
+    }
+}
+// display choices
+void displayChoices()
+{
+    printf("- \tcreate.\n"); 
+    printf("- \tread.\n"); 
+    printf("- \tedit.\n"); 
+    printf("- \tdelete.\n"); 
+    printf("= \texit.\n"); 
+}
 #endif
--- main/shelly_main.c
+++ main/shelly_main.c
@@ -4,8 +4,48 @@
 #include <strings.h>
 #include "../include/shelly_func.h"
 
+bool running = true;  
 
 int main()
 {
+    printf("Welcome to Shelly... a super basic file shell thing.\n");  
+    FILE *userFile; 
+    char *userInputCommand; 
+    char *userInput; 
+    do
+    {
+        displayChoices();
+        userInputCommand = getInput(); 
+        switch (commandType(userInputCommand))
+        {
+        case CREATE:
+            userInput = getInput(); 
+            userFile = createFile(userInput); 
+        break;
+        case READ:
+            userInput = getInput(); 
+            readFile(userInput, userFile);
+        break; 
+        case EDIT:
+            userInput = getInput(); 
+            userFile = editFile(userInput);
+        break; 
+        case DELETE:
+            userInput = getInput(); 
+            deleteFile(userInput); 
+        break; 
+        case EXT:
+            printf("Exiting...\n"); 
+            running = false; 
+        break;
+        default:
+            printf("Please choose a valid choice.\n"); 
+        break;
+        }
+    }while (running == true);
+    
+    
+
+
     return 0; 
 }

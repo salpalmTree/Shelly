89a86e5e681a9e7896b3109109a2a81c224f3a99
salpalmTree <salpalma123@gmail.com>
2023-09-05 18:02:06 -0600

    Functions moved from header file to c file

--- include/shelly_func.h
+++ include/shelly_func.h
@@ -2,8 +2,11 @@
 #define _SHELLY_FUNC_H
 #define SHELLY_GL_BUFSIZE 64
 #define MAX_FILE_AMOUNT 10
+#include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-bool running = false;  
 typedef enum command {
     EXT = 0,
     DELETE,
@@ -25,270 +28,31 @@ typedef struct dirNode {
     struct fileNode files[MAX_FILE_AMOUNT]; 
 }dirNode; 
 
-dirNode *head = NULL; 
 // read input as "create data.txt"
 // read input
-char * getInput(void)
-{
-    int buffsize = SHELLY_GL_BUFSIZE; 
-    int position = 0; 
-    int c; 
-    char *theInputString = (char *)malloc(sizeof(char) * buffsize);  
-    if(!theInputString) return NULL; 
-    while((c = getchar()) != '\n')
-    {
-        theInputString[position] = c; 
-        position++; 
-        if(position >= buffsize)
-        {
-            buffsize += SHELLY_GL_BUFSIZE; 
-            theInputString = realloc(theInputString, buffsize); 
-            if(!theInputString) return NULL; 
-        }
-    }
-    theInputString[position] = '\0'; 
-    return theInputString; 
-}
+char * getInput(void);
 // string comparison function
-bool equalStrings(char *stringOne, char *stringTwo)
-{
-    bool areEqual; 
-    (strcmp(stringOne, stringTwo) != 0 ? (areEqual = false) : (areEqual = true)); 
-    return areEqual; 
-}
+bool equalStrings(char *stringOne, char *stringTwo); 
 // sets command type
-aCommand commandType(char *userInputString)
-{
-    if(equalStrings(userInputString, "delete"))
-    {
-        free(userInputString);
-        return DELETE; 
-    }
-    else if(equalStrings(userInputString, "create"))
-    {
-        return CREATE;
-    }
-    else if(equalStrings(userInputString, "read"))
-    {
-        return READ; 
-    }
-    else if(equalStrings(userInputString, "edit"))
-    {
-        return EDIT;
-    }
-    else if(equalStrings(userInputString, "exit"))
-    {
-        return EXT; 
-    }
-    else
-    {
-        return CONT; 
-    }
-}
+aCommand commandType(char *userInputstring);
 // file creation
-FILE * createFile(char *fileName)
-{
-    FILE *newFile; 
-    if((newFile = fopen(fileName, "w")) == NULL) {
-        printf("File could not be created.\n");
-        free(fileName); 
-        return NULL;
-    }
-    else
-    {
-        printf("A file has been created %s\n", fileName); 
-        printf("To file ->"); 
-        //free(fileName); // Might free() too much
-        char *userIn = getInput(); 
-        fputs(userIn, newFile); 
-        fclose(newFile);
-        free(fileName); 
-        free(userIn); 
-        return newFile; 
-    }
-}
+FILE * createFile(char *fileName);
 // file deletion
-void deleteFile(char *fileName)
-{
-    if(remove(fileName))
-    {
-        printf("Could not delete that file.\n"); 
-    }
-    free(fileName); 
-}
+void deleteFile(char *fileName);
 // read file
-void readFile(char *fileName)
-{
-    FILE *fileToRead; 
-    int c;
-    if((fileToRead = fopen(fileName, "r")) == NULL) 
-    {
-        printf("File doesn't exit.\n"); 
-        free(fileName); 
-    }
-    else
-    { 
-        while((c = getc(fileToRead)) != EOF)
-        {
-            putchar(c); 
-        }
-        free(fileName); 
-        fclose(fileToRead); 
-    }
-}
+void readFile(char *fileName);
 // edit file (append)
-FILE * editFile(char *fileName)
-{
-    FILE *fileToAppend; 
-    if((fileToAppend = fopen(fileName, "a")) == NULL)
-    {
-        printf("That file could not be edited.\n");
-        free(fileName); 
-        return NULL;
-    }
-    else
-    { 
-        printf("File being appended %s\n", fileName); 
-        printf("To File ->"); 
-        free(fileName); 
-        char *userIn = getInput(); 
-        fputs(userIn, fileToAppend); 
-        free(userIn); 
-        fclose(fileToAppend); 
-        return fileToAppend; 
-    }
-}
+FILE * editFile(char *fileName);
 // display choices
-void displayChoices(void)
-{
-    printf("\n- \tcreate.\n"); 
-    printf("- \tread.\n"); 
-    printf("- \tedit.\n"); 
-    printf("- \tdelete.\n"); 
-    printf("= \texit.\n"); 
-}
-void add_Dir(dirNode **head, char *dirName)
-{
-    dirNode *dirToAdd = (dirNode*)malloc(sizeof(dirNode)); 
-    if(!dirToAdd)
-    {
-        printf("Error creating Directory\n"); 
-        free(dirToAdd); 
-    }
-    if(*head == NULL) // list is empty
-    {
-        dirToAdd->dirName = dirName; 
-        dirToAdd->next = *head; 
-        dirToAdd->prev = NULL;
-    }
-    else
-    {
-        dirToAdd->dirName = dirName; 
-        dirToAdd->next = *head; 
-        dirToAdd->prev = (*head)->prev; 
-         
-    }
-    for(int i = 0; i < MAX_FILE_AMOUNT; i++)
-    {
-        dirToAdd->files[i].fileName = "xemptyx"; 
-    }
-    *head = dirToAdd;
-}
-
-dirNode * lookup_Dir(dirNode *head, char *dirName)
-{
-    while(head != NULL)
-    {
-        if(strcmp(head->dirName, dirName) == 0)
-        {
-            return head; 
-        }
-        head = head->next; 
-    }
-    printf("Error finding that Direcotry.\n"); 
-    return NULL; 
-}
-
-void add_File_To_Dir(dirNode *head, char *fileName)
-{
-    bool createdFile = false; 
-    if(head == NULL)
-    {
-        printf("Directory not found.\n");
-    }
-    else
-    {
-        while(!createdFile)
-        {
-            for(int i = 0; i < MAX_FILE_AMOUNT; i++)
-            {
-                if((strcmp(head->files[i].fileName, "xemptyx")) == 0)
-                {
-                    if(!(head->files[i].file = createFile(fileName)))
-                    {
-                        printf("File could not be created.\n"); 
-                    }
-                    else
-                    {
-                        head->files[i].fileName = fileName; 
-                        printf("File created in %s directory as %s \n", head->dirName, head->files[i].fileName); 
-                        createdFile = true; 
-                        break; 
-                    }
-                }
-            }
-        }
-    }
-}
-
-void ls_Dir(dirNode *head)
-{
-    int i = 0; 
-    int fileCount = 0; 
-    if(head == NULL)
-    {
-        printf("No Directory.\n"); 
-    }
-    else
-    {
-        printf("Name of Directory: %s\n", head->dirName);
-        if(head->next != NULL)
-        {
-            printf("%s.dir\n", head->next->dirName); 
-        }
-        while(i < MAX_FILE_AMOUNT)
-        {
-            if((strcmp(head->files[i].fileName, "xemptyx")) != 0)
-            {
-                printf("%s\n", head->files[i].fileName);
-                fileCount++; 
-            }
-            i++; 
-        }
-    }
-}
-
-void printDir(dirNode *head)
-{
-    if(head == NULL)
-    {
-        printf("The List is empty.\n"); 
-    }
-    else
-    {
-        while(head != NULL)
-        {
-            if(head->prev == NULL)
-            {
-                printf("NULL -> "); 
-            }
-            printf("%s -> ", head->dirName); 
-            head = head->next; 
-        }
-        if(head == NULL)
-        {
-            printf("NULL"); 
-        }
-    }
-}
+void displayChoices(void);
+// add a directory
+void add_Dir(dirNode **head, char *dirName);
+// look up a directory 
+dirNode * lookup_Dir(dirNode *head, char *dirName);
+// add file to directory
+void add_File_To_Dir(dirNode *head, char *fileName);
+// list a directory
+void ls_Dir(dirNode *head);
+// print a Directory for debugging
+void printDir(dirNode *head);
 #endif
--- main/shelly_main.c
+++ main/shelly_main.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <strings.h>
 #include "../include/shelly_func.h"
 
+bool running = false;  
+dirNode *head = NULL; 
 int main()
 {
     add_Dir(&head, getInput());
@@ -52,3 +50,272 @@ int main()
 
     return 0; 
 }
+
+char * getInput(void)
+{
+    int buffsize = SHELLY_GL_BUFSIZE; 
+    int position = 0; 
+    int c; 
+    char *theInputString = (char *)malloc(sizeof(char) * buffsize);  
+    if(!theInputString) return NULL; 
+    while((c = getchar()) != '\n')
+    {
+        theInputString[position] = c; 
+        position++; 
+        if(position >= buffsize)
+        {
+            buffsize += SHELLY_GL_BUFSIZE; 
+            theInputString = (char*)realloc(theInputString, buffsize); 
+            if(!theInputString) return NULL; 
+        }
+    }
+    theInputString[position] = '\0'; 
+    return theInputString; 
+}
+
+bool equalStrings(char *stringOne, char *stringTwo)
+{
+    bool areEqual; 
+    (strcmp(stringOne, stringTwo) != 0 ? (areEqual = false) : (areEqual = true)); 
+    return areEqual; 
+}
+
+aCommand commandType(char *userInputString)
+{
+    if(equalStrings(userInputString, "delete"))
+    {
+        free(userInputString);
+        return DELETE; 
+    }
+    else if(equalStrings(userInputString, "create"))
+    {
+        return CREATE;
+    }
+    else if(equalStrings(userInputString, "read"))
+    {
+        return READ; 
+    }
+    else if(equalStrings(userInputString, "edit"))
+    {
+        return EDIT;
+    }
+    else if(equalStrings(userInputString, "exit"))
+    {
+        return EXT; 
+    }
+    else
+    {
+        return CONT; 
+    }
+}
+
+
+FILE * createFile(char *fileName)
+{
+    FILE *newFile; 
+    if((newFile = fopen(fileName, "w")) == NULL) {
+        printf("File could not be created.\n");
+        free(fileName); 
+        return NULL;
+    }
+    else
+    {
+        printf("A file has been created %s\n", fileName); 
+        printf("To file ->"); 
+        //free(fileName); // Might free() too much
+        char *userIn = getInput(); 
+        fputs(userIn, newFile); 
+        fclose(newFile);
+        free(fileName); 
+        free(userIn); 
+        return newFile; 
+    }
+}
+
+void deleteFile(char *fileName)
+{
+    if(remove(fileName))
+    {
+        printf("Could not delete that file.\n"); 
+    }
+    free(fileName); 
+}
+
+void readFile(char *fileName)
+{
+    FILE *fileToRead; 
+    int c;
+    if((fileToRead = fopen(fileName, "r")) == NULL) 
+    {
+        printf("File doesn't exit.\n"); 
+        free(fileName); 
+    }
+    else
+    { 
+        while((c = getc(fileToRead)) != EOF)
+        {
+            putchar(c); 
+        }
+        free(fileName); 
+        fclose(fileToRead); 
+    }
+}
+
+FILE * editFile(char *fileName)
+{
+    FILE *fileToAppend; 
+    if((fileToAppend = fopen(fileName, "a")) == NULL)
+    {
+        printf("That file could not be edited.\n");
+        free(fileName); 
+        return NULL;
+    }
+    else
+    { 
+        printf("File being appended %s\n", fileName); 
+        printf("To File ->"); 
+        free(fileName); 
+        char *userIn = getInput(); 
+        fputs(userIn, fileToAppend); 
+        free(userIn); 
+        fclose(fileToAppend); 
+        return fileToAppend; 
+    }
+}
+
+void displayChoices(void)
+{
+    printf("\n- \tcreate.\n"); 
+    printf("- \tread.\n"); 
+    printf("- \tedit.\n"); 
+    printf("- \tdelete.\n"); 
+    printf("= \texit.\n"); 
+}
+
+void add_Dir(dirNode **head, char *dirName)
+{
+    dirNode *dirToAdd = (dirNode*)malloc(sizeof(dirNode)); 
+    if(!dirToAdd)
+    {
+        printf("Error creating Directory\n"); 
+        free(dirToAdd); 
+    }
+    if(*head == NULL) // list is empty
+    {
+        dirToAdd->dirName = dirName; 
+        dirToAdd->next = *head; 
+        dirToAdd->prev = NULL;
+    }
+    else
+    {
+        dirToAdd->dirName = dirName; 
+        dirToAdd->next = *head; 
+        dirToAdd->prev = (*head)->prev; 
+         
+    }
+    for(int i = 0; i < MAX_FILE_AMOUNT; i++)
+    {
+        dirToAdd->files[i].fileName = "xemptyx"; 
+    }
+    *head = dirToAdd;
+}
+
+dirNode * lookup_Dir(dirNode *head, char *dirName)
+{
+    while(head != NULL)
+    {
+        if(strcmp(head->dirName, dirName) == 0)
+        {
+            return head; 
+        }
+        head = head->next; 
+    }
+    printf("Error finding that Direcotry.\n"); 
+    return NULL; 
+}
+
+void add_File_To_Dir(dirNode *head, char *fileName)
+{
+    bool createdFile = false; 
+    if(head == NULL)
+    {
+        printf("Directory not found.\n");
+    }
+    else
+    {
+        while(!createdFile)
+        {
+            for(int i = 0; i < MAX_FILE_AMOUNT; i++)
+            {
+                if((strcmp(head->files[i].fileName, "xemptyx")) == 0)
+                {
+                    if(!(head->files[i].file = createFile(fileName)))
+                    {
+                        printf("File could not be created.\n"); 
+                    }
+                    else
+                    {
+                        head->files[i].fileName = fileName; 
+                        printf("File created in %s directory as %s \n", head->dirName, head->files[i].fileName); 
+                        createdFile = true; 
+                        break; 
+                    }
+                }
+            }
+        }
+    }
+}
+
+void ls_Dir(dirNode *head)
+{
+    int i = 0; 
+    int fileCount = 0; 
+    if(head == NULL)
+    {
+        printf("No Directory.\n"); 
+    }
+    else
+    {
+        printf("Name of Directory: %s\n", head->dirName);
+        if(head->next != NULL)
+        {
+            printf("%s.dir\n", head->next->dirName); 
+        }
+        while(i < MAX_FILE_AMOUNT)
+        {
+            if((strcmp(head->files[i].fileName, "xemptyx")) != 0)
+            {
+                printf("%s\n", head->files[i].fileName);
+                fileCount++; 
+            }
+            i++; 
+        }
+    }
+}
+
+void printDir(dirNode *head)
+{
+    if(head == NULL)
+    {
+        printf("The List is empty.\n"); 
+    }
+    else
+    {
+        while(head != NULL)
+        {
+            if(head->prev == NULL)
+            {
+                printf("NULL -> "); 
+            }
+            printf("%s -> ", head->dirName); 
+            head = head->next; 
+        }
+        if(head == NULL)
+        {
+            printf("NULL"); 
+        }
+    }
+}
+
+
+
